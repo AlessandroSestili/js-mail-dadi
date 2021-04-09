2a2fc58bdcc466757ca704ce61a7e908893f53ce
AlessandroSestili <sestilialessandro@gmail.com>
2021-04-09 16:06:46 +0200

    fine progetto dadi

---
+++ dadi/dadi.js
@@ -0,0 +1,19 @@
+var mailUtente = prompt("Digita la tua mail")
+var mailValide = ["sestilialessandro@gmail.com" , "asestilz@gmail5.com" , "cocomeri@gmail.com" , "cani_volanti@gmail.com" , "sanguisughe-spruzzanti_inchiostro@yahoo.it"]
+
+var mailExists = false
+
+for (var i = 0; i < mailValide.length; i++) {
+
+    if(mailValide[i] === mailUtente) {
+        mailExists = true
+        alert("La tua Mail è corretta. ")
+    }
+}
+
+mailExists
+
+
+if(!mailExists) {
+    alert("Inserisci una mail valida.")
+}
